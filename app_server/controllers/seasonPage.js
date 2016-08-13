aa1d31c6f18f34b7da7eae17f6c4477e99ac644d
b0mberb0b <ns_miticholas@yahoo.com>
2016-08-13 02:49:31 -0700

    2nd batch css work

--- app_server/controllers/seasonPage.js
+++ app_server/controllers/seasonPage.js
@@ -1,4 +1,4 @@
 /* Function that renders season page when executed by Router. */
 module.exports.getPage = function(req, res, next) {
-  res.render('index', { title: 'Schmeater Season' });
+  res.render('season', { title: 'Schmeater Season' });
 };
--- app_server/controllers/supportPage.js
+++ app_server/controllers/supportPage.js
@@ -1,4 +1,4 @@
 /* Function that renders support page when executed by Router. */
 module.exports.getPage = function(req, res, next) {
-  res.render('index', { title: 'Support the Schmee' });
+  res.render('support', { title: 'Schmeater Season' });
 };
