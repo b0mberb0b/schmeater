1a5d0334ec2ef7d2c1c10295e5e033655ded525d
b0mberb0b <ns_miticholas@yahoo.com>
2016-09-03 00:58:00 -0700

    support page works all sizes

--- src/js/script.js
+++ src/js/script.js
@@ -3,3 +3,25 @@ $(document).on('click','.navbar-collapse.in',function(e) {
         $(this).collapse('hide');
     }
 });
+$('body').bind('click', function(e) {
+    if($(e.target).closest('.navbar').length == 0) {
+        // click happened outside of .navbar, so hide
+        var opened = $('.navbar-collapse').hasClass('collapse in');
+        if ( opened === true ) {
+            $('.navbar-collapse').collapse('hide');
+        }
+    }
+});
+// var subNav = $('#subNav');
+// $(window).resize(function() {
+//   if( subNav.css('position') == 'fixed' ){
+//     subNav.detach().appendTo($('#sideNav'));
+//     subNav.removeAttr('data-spy data-offset-top');
+//     subNav.removeClass('affix-top');
+//     $
+//     console.log("made it");
+//   } else {
+//     subNav.detach().appendTo($('#topNav'));
+//     subNav.attr({'data-spy': 'affix', 'data-offset-top': '175'});
+//   }
+// });
