aa8396a64db48303bea1eff36264416701971683
b0mberb0b <ns_miticholas@yahoo.com>
2016-10-05 21:43:06 -0700

    scaling seasons page

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,10 @@
+//
 $(document).on('click','.navbar-collapse.in',function(e) {
     if( $(e.target).is('a') ) {
         $(this).collapse('hide');
     }
 });
+//closes open hamburger menus when you click outside the menu
 $('body').bind('click', function(e) {
     if($(e.target).closest('.navbar').length == 0) {
         // click happened outside of .navbar, so hide
@@ -12,16 +14,11 @@ $('body').bind('click', function(e) {
         }
     }
 });
-// var subNav = $('#subNav');
-// $(window).resize(function() {
-//   if( subNav.css('position') == 'fixed' ){
-//     subNav.detach().appendTo($('#sideNav'));
-//     subNav.removeAttr('data-spy data-offset-top');
-//     subNav.removeClass('affix-top');
-//     $
-//     console.log("made it");
-//   } else {
-//     subNav.detach().appendTo($('#topNav'));
-//     subNav.attr({'data-spy': 'affix', 'data-offset-top': '175'});
-//   }
-// });
+//dynamically changes event tab paragraph size so ellipses look good
+var eventText = $('.eventText');
+$( window ).bind('resize', function(){
+    var width = $('.eventItem').width();
+    eventText.width( width - 175 );
+});
+
+eventText.width( $('.eventItem').width() - 175 );
